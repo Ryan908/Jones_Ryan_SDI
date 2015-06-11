b80ef4f5a1ed9158a964636f9a065e8fbb48b8c6
Ryan908 <rajones@fullsail.com>
2015-06-11 06:04:08 -0400

    Prompted user for name
    Prompted user for name and said welcome

--- GoToTrainingWeek2/js/week2.js
+++ GoToTrainingWeek2/js/week2.js
@@ -6,3 +6,11 @@ GoTo Training Week #2
  */
 
 //alert("Test to see if connected.");
+
+//Create an age calculator
+
+//Ask the user for their name
+var name = prompt("Let's figure out how old you are!\n Please type in your name");
+
+//Use the variable and say hello
+alert("Welcome "+name+"!");
