f445fe39a0e4f0eb85a162d2cab05b2c6ad4afe9
Ryan908 <rajones@fullsail.com>
2015-06-11 23:07:03 -0400

    Completed js assignment
    Completed js assignment

--- GoToTrainingWeek2/js/week2.js
+++ GoToTrainingWeek2/js/week2.js
@@ -2,33 +2,45 @@
 Ryan Jones
 SDI Section #2
 GoTo Training Week #2
-6/10/2015
+6/11/2015
  */
 
 //alert("Test to see if connected.");
 
-//Create an age calculator
+//Create an sale calculator
 
-//Ask the user for their name
-var name = prompt("Let's figure out how old you are!\n Please type in your name");
+//Ask the user what they would like to buy
+var item = prompt("What can I get for you today?");
 
-//Use the variable and say hello
-alert("Welcome "+name+"!");
+//Use the variable and confirm item for purchase
+alert("Great "+item+"!");
 
-//Ask the user what year they were born
-var yearBorn = prompt("What year were you born?");
+//Ask user for quantity
+var itemQuantity = prompt("How many do you wish to buy?");
 
 //Console.log the answer to check it
-console.log (yearBorn);
+console.log (itemQuantity);
 
-//Calculate their age
+//Calculate their total
 
-//We need to know the current year
+//We need to know the cost of the item
 //Hardcoded means- Type in a specific value
-var currentYear=2015;
+var itemPrice= 65;
 
-//Figure out age
-var age = currentYear - yearBorn;
+//Figure out total
+var total = itemPrice * itemQuantity;
 
 //Alert the user with their age.
-alert(name+ " You are "+age+" years old");
+alert("Your "+item+" will be "+total+ " dollars");
+console.log ("Your "+item+" will be "+total+ " dollars");
+
+var change =  prompt("How much would you like to put toward your bill?");
+
+console.log (change);
+
+//New total is total + more items
+//Cast the variable as a number when using +
+var newTotal = parseInt(change) - total ;
+
+//Console.log and alert the final values
+console.log (newTotal+ " dollars is your change");
