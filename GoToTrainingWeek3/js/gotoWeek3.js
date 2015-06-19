52d4e7ad6bbcfccc753f5ead121d0fb22748110c
Ryan908 <rajones@fullsail.com>
2015-06-18 23:23:53 -0400

    Conditions assignment
    Conditions assignment

--- GoToTrainingWeek3/js/gotoWeek3.js
+++ GoToTrainingWeek3/js/gotoWeek3.js
@@ -7,3 +7,13 @@ Go To Week #3
  */
 
  //alert ("Testing to see if connected")
+
+ //Calculate if we will play 21 or 5 on 5.
+ var numPlayers = ("10");
+
+console.log ("numPlayers");
+
+ var numPlayers = prompt("How man players do you have?");
+ 
+
+
