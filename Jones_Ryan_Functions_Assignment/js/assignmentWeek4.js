a559a271868d12f2c67756c014091d4c6616dae9
Ryan908 <rajones@fullsail.com>
2015-06-24 22:34:08 -0400

    Created function with prompts
    Prompted user and created a function that included 3 parameters with
    arguments.

--- Jones_Ryan_Functions_Assignment/js/assignmentWeek4.js
+++ Jones_Ryan_Functions_Assignment/js/assignmentWeek4.js
@@ -8,4 +8,24 @@ Jones_Ryan_Functions_Assignment
 
  //alert("This is a test")
 
- 
+//Asking user to confirm number of guests that will be attending their event.
+//Prompt requesting on number of vip reservations,other attendees and staff.
+ var vip= prompt("How many reservations do you have for vip?");
+ var other= prompt("How many other attendees will there be?");
+ var staff= prompt("How many staff members will be present?");
+
+
+//Function call including 3 arguments
+
+ var attendance1 = calcAttendance(vip,other,staff);
+
+//Function including parameters
+ function calcAttendance(vip,other,staff){
+
+     var total = Number(vip)+Number(other)+Number(staff);
+
+     return total;
+
+     console.log("You will have"+total+"guests and staff present at this event.")
+
+ }
