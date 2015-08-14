4f3aed3c87f4ba0df1a03d38b025daa2e325aa4e
Ryan908 <rajones@fullsail.com>
2015-08-13 20:57:45 -0400

    Prompted user to determine weight

---
+++ Jones_Ryan_Expressions_Assignment/js/expressionsAssignment.js
@@ -0,0 +1,13 @@
+/**
+ * Created by ryanjones on 8/13/15.
+ */
+
+//alert ("Testing to see if connected.");
+
+//This will be a weight loss calculator.
+
+//prompt user asking current weight + weight for the previous two months
+
+var weight1 = prompt("How much weight did you lose for the month of August?");
+var weight2 = prompt("How much weight did you lose in July?");
+var weight3 = prompt("How much weight did you lose June?");
