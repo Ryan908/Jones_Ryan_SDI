e7ae029c7234d7937c6f162d70d4ea604ff5764b
Ryan908 <rajones@fullsail.com>
2015-08-20 22:39:11 -0400

    Added another if conditional statement

--- Jones_Ryan_Conditionals_Assignment/js/conditionalsAssignmentWk3.js
+++ Jones_Ryan_Conditionals_Assignment/js/conditionalsAssignmentWk3.js
@@ -7,19 +7,24 @@
 
 var pizzaParty= prompt("How many will be attending your pizza party?");
 var pizzaPies= prompt("How many pies will you order?");
-
+var notEnough= 1;
 
 var totalPies = pizzaParty / pizzaPies;
 console.log(totalPies);
 
-if(totalPies >= pizzaPies){
+if(pizzaParty < pizzaPies){
     console.log("You do not have enough pizza for you guests.");
-}else if(totalPies > pizzaParty){
-    console.log("That's a lot of pizza.");
+}else if(totalPies === notEnough){
+    console.log("You may need more pies.");
 }else{
     console.log("You have enough pizza.");
 }
 
+ if(totalPies<notEnough){
+     console.log("That's not enough pizza for one person.");
+ }else{
+     console.log("That should be enough to satisfy one person.");
+ }
 
 
 
