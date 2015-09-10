295bba30f67575eecca20045a469f4c1c37de488
Ryan908 <rajones@fullsail.com>
2015-09-09 20:21:42 -0400

    prompted user twice using variables
    use variables in two prompts and alerted user with variable confirming
    they’re input

--- Jones Ryan Expression Assignment/js/Jones_Ryan_Expressions_Assignment.js
+++ Jones Ryan Expression Assignment/js/Jones_Ryan_Expressions_Assignment.js
@@ -1,1 +1,19 @@
 
+
+
+
+
+
+
+
+//Created variable students and prompted user for input
+
+var students = prompt("How many students are in your class?");
+
+//Alert user confirming number of students and advise to the next step
+alert("Ok you will have " +students+ " students in attendance. \nNext we will need to find out how many cupcakes each student will have.");
+
+var cupcakes = prompt("How many cupcakes will each of your student be allowed to have?");
+
+
+
