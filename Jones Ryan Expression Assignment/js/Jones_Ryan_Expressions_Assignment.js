193863d9dcf82cf7319871698c65fe0bbf32f400
Ryan908 <rajones@fullsail.com>
2015-09-09 20:58:54 -0400

    Multiplied variables and printed the total
    Multiplied the number of students the how many cakes each would have
    and printed the total  number of cupcakes needed to the console.

--- Jones Ryan Expression Assignment/js/Jones_Ryan_Expressions_Assignment.js
+++ Jones Ryan Expression Assignment/js/Jones_Ryan_Expressions_Assignment.js
@@ -1,4 +1,9 @@
-
+/*
+Ryan Jones
+SDI Section 02
+09/09/2015
+Expressions Assignment
+ */
 
 
 
@@ -10,10 +15,24 @@
 
 var students = prompt("How many students are in your class?");
 
+//Print number of student to the console.
+console.log(students);
+
 //Alert user confirming number of students and advise to the next step
 alert("Ok you will have " +students+ " students in attendance. \nNext we will need to find out how many cupcakes each student will have.");
 
+//Created variable cupcakes and prompted for user input.
 var cupcakes = prompt("How many cupcakes will each of your student be allowed to have?");
+
+//Print number of cupcakes to the console
+console.log(cupcakes);
+
+//Multiplied the number of students to get the total number of cupcakes needed.
+students *= cupcakes;
+
+//Printed the total number of cupcakes to the console
+console.log(students);
+
 
 
 
