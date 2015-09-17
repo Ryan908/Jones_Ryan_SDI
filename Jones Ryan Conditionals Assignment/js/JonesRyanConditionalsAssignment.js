0fbeb2749cd78da7849c339dd20582e611651ed9
Ryan908 <rajones@fullsail.com>
2015-09-16 20:16:40 -0400

    Created files
    created js and html files and created first variable

---
+++ Jones Ryan Conditionals Assignment/js/JonesRyanConditionalsAssignment.js
@@ -0,0 +1,11 @@
+/*
+Ryan Jones
+SDI Section 02
+Conditionals Assignment
+09/16/15
+ */
+
+//set up files and first variable of maximum capacity of guests allowed on the museum tour.
+var tourGuestMax = 10
+
+
