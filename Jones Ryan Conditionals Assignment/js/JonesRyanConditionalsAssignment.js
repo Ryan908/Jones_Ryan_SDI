5c193a2ab3c277e5c79d5fe82b85ae8f1d300403
Ryan908 <rajones@fullsail.com>
2015-09-17 20:12:48 -0400

    Calculated string values and validated entry
    Calculated string values and validated entry

--- Jones Ryan Conditionals Assignment/js/JonesRyanConditionalsAssignment.js
+++ Jones Ryan Conditionals Assignment/js/JonesRyanConditionalsAssignment.js
@@ -6,6 +6,16 @@ Conditionals Assignment
  */
 
 //set up files and first variable of maximum capacity of guests allowed on the museum tour.
-var tourGuestMax = 10
+var tourGuestMax = 10;
 
+//prompt user for the number of guests.
+var guests = prompt("How many guests would you like to invite on your tour?");
+
+//Prompt user whether they have an adequate number of guests and validate entry.
+(guests <= tourGuestMax) ? console.log("Ok let's begin the tour!") : console.log("You have too many people.");
+
+if (guests === ""){
+    console.log("You must enter a number!");
+    alert("You must enter a number!");
+}
 
