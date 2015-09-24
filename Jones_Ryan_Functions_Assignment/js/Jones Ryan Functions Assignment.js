d85e36dc1473639c2ce2c1a5ff775b52a4981350
Ryan908 <rajones@fullsail.com>
2015-09-23 22:24:23 -0400

    Created and called first function
    Created function calculating parameters and arguments. Called function

--- Jones_Ryan_Functions_Assignment/js/Jones Ryan Functions Assignment.js
+++ Jones_Ryan_Functions_Assignment/js/Jones Ryan Functions Assignment.js
@@ -7,9 +7,27 @@ Functions Assignment
 
 
 //Prompted user to enter value for first variable and validated using while loop.
-var a = prompt("How do the pair of jeans you wish to buy cost?");
+var a = prompt("How much do the pair of jeans you wish to buy cost?");
 
 while(a===""){
     console.log("Please enter the cost to continue");
     a= prompt("Please enter the cost to continue");
 }
+
+//Setup second variable and validated using while loop.
+var b = prompt("How many pairs do you wish to buy?");
+
+while(b===""){
+    console.log("Please enter how many pairs you wish do buy. \n Do not leave blank.");
+    b= prompt("Please enter how many pairs you wish do buy. \n Do not leave blank.");
+}
+//Calling function
+retailCalc(a,b);
+
+//Created function and calculated variable values.
+function retailCalc ( a,b ){
+    retailCalc = a * b;
+    console.log(retailCalc);
+
+}
+
