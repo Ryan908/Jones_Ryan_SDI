0ea7d4c3b5d81547388e25b0c841550e49619876
Ryan908 <rajones@fullsail.com>
2015-09-24 20:39:49 -0400

    Created variables for second function
    Created and validated 3 variables for second function.

--- Jones_Ryan_Functions_Assignment/js/Jones Ryan Functions Assignment.js
+++ Jones_Ryan_Functions_Assignment/js/Jones Ryan Functions Assignment.js
@@ -21,13 +21,38 @@ while(b===""){
     console.log("Please enter how many pairs you wish do buy. \n Do not leave blank.");
     b= prompt("Please enter how many pairs you wish do buy. \n Do not leave blank.");
 }
-//Calling function
+//Calling function with arguments
 retailCalc(a,b);
 
-//Created function and calculated variable values.
+//Created function parameters.
 function retailCalc ( a,b ){
     retailCalc = a * b;
     console.log(retailCalc);
+    return retailCalc
 
+}
+
+//Prompting user to commit values to variables that will be used in second function.
+prompt("You should buy some other items to go with your new jeans. \n How about a new shirt, a jacket and a new pair of shoes");
+
+var c= prompt("How much is your shirt?");
+
+while(c===""){
+    console.log("Please enter the price of your shirt.");
+    c= prompt("Please enter the price of your shirt.");
+}
+
+var d = prompt("How much is the jacket?");
+
+while(d=== ""){
+    console.log("Please enter the price of the jacket.");
+    d = prompt("Please enter the price of the jacket.");
+}
+
+var e = prompt("How much are is the pair of shoes that you plan to buy?");
+
+while(e===""){
+    console.log("Please enter the price of the shoe you are buying.");
+    e = prompt("Please enter the price of the shoe you are buying.");
 }
 
